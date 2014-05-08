46cd4005c22961b266f2a5032e2d0a5d99b5801f
Glenn Searby <glenn.searby@gmail.com>
2014-05-08 10:23:28 +0100

    Dmenu config for xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,7 +1,11 @@
 import XMonad
 import XMonad.Config.Gnome
+import XMonad.Util.EZConfig (additionalKeysP)
 
 main = do
-    xmonad $ gnomeConfig { 
-        modMask = mod4Mask
-    }
+    xmonad $ 
+               (gnomeConfig { 
+                  modMask = mod4Mask
+                }
+                `additionalKeysP`
+                [("M-p", spawn "dmenu_run")])
