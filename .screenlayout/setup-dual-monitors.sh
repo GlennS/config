9a05061863c1cbc8dde1154029f70978de67bc57
Glenn Searby <glenn.searby@gmail.com>
2015-11-13 10:24:15 +0000

    Dual monitors on home PC

--- .screenlayout/setup-dual-monitors.sh
+++ .screenlayout/setup-dual-monitors.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI1 --off --output DP1 --mode 1680x1050 --pos 0x30 --rotate normal --output eDP1 --mode 1920x1080 --pos 1680x0 --rotate normal --output VIRTUAL1 --off
+xrandr --output DisplayPort-0 --off --output DVI-1 --mode 2560x1440 --pos 1280x0 --rotate normal --output DVI-0 --mode 1280x1024 --pos 0x0 --rotate normal --output HDMI-0 --off
