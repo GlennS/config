770cb60de94d3dd57cc381434c34e8bd84b6a2f3
Glenn Searby <glenn.searby@gmail.com>
2016-08-08 10:32:45 +0100

    Switched my work monitors around

--- .screenlayout/setup-dual-monitors.sh
+++ .screenlayout/setup-dual-monitors.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI1 --off --output DP1 --mode 1680x1050 --pos 0x30 --rotate normal --output eDP1 --mode 1920x1080 --pos 1680x0 --rotate normal --output VIRTUAL1 --off
+xrandr --output HDMI1 --off --output DP1 --mode 1680x1050 --pos 1920x30 --rotate normal --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal
