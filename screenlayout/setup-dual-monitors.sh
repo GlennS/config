d2d9be3e46680ecd367501eb31f053c50fc91a28
Glenn Searby <glenn.searby@gmail.com>
2017-03-30 16:29:33 +0100

    Dual monitors vary by computer

--- screenlayout/setup-dual-monitors.sh
+++ screenlayout/setup-dual-monitors.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-xrandr --output HDMI1 --off --output DP1 --mode 1680x1050 --pos 1920x30 --rotate normal --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal
+
+if test "$(hostname)" = "jesus-pc"; then
+    xrandr --output DisplayPort-0 --off --output DVI-1 --primary --mode 2560x1440 --pos 1280x0 --rotate normal --output DVI-0 --mode 1280x1024 --pos 0x0 --rotate normal --output HDMI-0 --off
+fi
+
+if test "$(hostname)" = "glenn-work"; then
+    xrandr --output HDMI1 --off --output DP1 --mode 1680x1050 --pos 1920x30 --rotate normal --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal
+fi
