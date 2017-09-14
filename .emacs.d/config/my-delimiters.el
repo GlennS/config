f1d90d6483968572084cc9285b60fc5cc16023ec
Glenn Searby <glenn.searby@gmail.com>
2017-09-14 17:13:31 +0100

    Better shortcuts for delimiters:  * M-cursor navigates  * M-S-cursor manipulates  * Removed some commands which appear to do the same thing

--- .emacs.d/config/my-delimiters.el
+++ .emacs.d/config/my-delimiters.el
@@ -6,19 +6,16 @@
 (add-hook 'lisp-interaction-mode-hook #'smartparens-mode)
 (add-hook 'java-mode #'smartparens-mode)
 
-;; TODO: these are rubbish key bindings that I don't understand, try again
-(define-key smartparens-mode-map (kbd "C-M-<left>") 'sp-backward-sexp)
-(define-key smartparens-mode-map (kbd "C-M-<right>") 'sp-forward-sexp)
-(define-key smartparens-mode-map (kbd "C-M-<up>") 'sp-up-sexp)
-(define-key smartparens-mode-map (kbd "C-M-<down>") 'sp-down-sexp)
+;; Some key bindings for working with delimiters
+(define-key smartparens-mode-map (kbd "M-<left>") 'sp-backward-sexp)
+(define-key smartparens-mode-map (kbd "M-<right>") 'sp-forward-sexp)
+(define-key smartparens-mode-map (kbd "M-<up>") 'sp-up-sexp)
+(define-key smartparens-mode-map (kbd "M-<down>") 'sp-down-sexp)
 
-(define-key smartparens-mode-map (kbd "M-<left>") 'sp-backward-slurp-sexp)
-(define-key smartparens-mode-map (kbd "M-<right>") 'sp-forward-slurp-sexp)
-(define-key smartparens-mode-map (kbd "M-<up>") 'sp-add-to-previous-sexp)
-(define-key smartparens-mode-map (kbd "M-<down>") 'sp-add-to-next-sexp)
-
-(define-key smartparens-mode-map (kbd "S-M-<left>") 'sp-forward-barf-sexp)
-(define-key smartparens-mode-map (kbd "S-M-<right>") 'sp-backward-barf-sexp)
+(define-key smartparens-mode-map (kbd "S-M-<left>") 'sp-backward-slurp-sexp)
+(define-key smartparens-mode-map (kbd "S-M-<right>") 'sp-forward-slurp-sexp)
+(define-key smartparens-mode-map (kbd "S-M-<up>") 'sp-backward-barf-sexp)
+(define-key smartparens-mode-map (kbd "S-M-<down>") 'sp-forward-barf-sexp)
 
 (define-key smartparens-mode-map (kbd "C-M-w") 'sp-copy-sexp)
 (define-key smartparens-mode-map (kbd "C-M-k") 'sp-kill-sexp)
