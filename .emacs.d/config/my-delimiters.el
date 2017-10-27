ee5052accef5155570b21a1f284b6ba880551027
Glenn Searby <glenn.searby@gmail.com>
2017-10-27 11:43:22 +0100

    Better paren colours

--- .emacs.d/config/my-delimiters.el
+++ .emacs.d/config/my-delimiters.el
@@ -31,12 +31,21 @@
                     :inherit 'error
                     :strike-through t)
 
-;; Saturated colours for rainbow delimiters
-(require 'color)
-(cl-loop
- for index from 1 to rainbow-delimiters-max-face-count
- do
- (let ((face (intern (format "rainbow-delimiters-depth-%d-face" index))))
-   (cl-callf color-saturate-name (face-foreground face) 100)))
+;; Colours for rainbow delimiters
+;; Taken from d3.js scheme set 1, with the grey and brown removed
+;; https://github.com/d3/d3-scale-chromatic
+;; https://github.com/d3/d3-scale-chromatic/blob/master/src/categorical/Set1.js
+(let ((colours '("#e41a1c" "#377eb8" "#4daf4a" "#984ea3" "#ff7f00" "#ffff33" "#f781bf"))
+      (i 1)
+      )
+  (dolist (c colours)
+
+    (set-face-attribute (intern (format "rainbow-delimiters-depth-%d-face" i))
+                        nil
+                        :foreground c)
+
+    (cl-incf i)
+    )
+  (setq rainbow-delimiters-max-face-count (- i 1)))
 
 (provide `my-delimiters)
