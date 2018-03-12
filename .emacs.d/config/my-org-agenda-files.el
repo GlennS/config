3b81985820610900d80c6a8c7f13ca973c49502f
Glenn Searby <glenn.searby@gmail.com>
2018-03-12 11:21:29 +0000

    Removed my old config

--- .emacs.d/config/my-org-agenda-files.el
+++ .emacs.d/config/my-org-agenda-files.el
@@ -1,19 +1,7 @@
-(let ((home-agenda-files '("~/notes/todo.org"
-                            "~/notes/reading.org"
-                            "~/notes/schedule.org"))
-
-      (project-agenda-files '())
-
-      (work-agenda-files '("~/work/todo.org"
-                           "~/work/events.org")))
-
-  (setq org-agenda-files
-        (if (string= system-name "glenn-laptop")
-
-            (append
-             work-agenda-files
-             project-agenda-files)
-
-          home-agenda-files)))
+(setq org-agenda-files '("~/notes/todo.org"
+                        "~/notes/reading.org"
+                        "~/notes/schedule.org"
+                        "~/work/todo.org"
+                        "~/work/events.org"))
 
 (provide `my-org-agenda-files)
--- screenlayout/setup-dual-monitors.sh
+++ screenlayout/setup-dual-monitors.sh
@@ -5,12 +5,6 @@ GENERATED_SCRIPT="/usr/share/lightdm-dual-monitors.sh"
 
 echo "display-setup-script = ${GENERATED_SCRIPT}" | sudo tee --append /etc/lightdm/lightdm.conf
 
-if test "$(hostname)" = "jesus-pc"; then
-    echo "xrandr --output DisplayPort-0 --off --output DVI-1 --primary --mode 2560x1440 --pos 1280x0 --rotate normal --output DVI-0 --mode 1280x1024 --pos 0x0 --rotate normal --output HDMI-0 --off" | sudo tee "${GENERATED_SCRIPT}"
-fi
-
-if test "$(hostname)" = "glenn-laptop"; then
-    echo "xrandr --output HDMI1 --off --output DP1 --mode 1680x1050 --pos 1920x30 --rotate normal --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal" | sudo tee "${GENERATED_SCRIPT}"
-fi
+echo "xrandr --output HDMI1 --off --output DP1 --mode 1680x1050 --pos 1920x30 --rotate normal --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal" | sudo tee "${GENERATED_SCRIPT}"
 
 sudo chmod u+x "${GENERATED_SCRIPT}"
