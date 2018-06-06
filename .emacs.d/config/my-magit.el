15df9d625a5239581fcc50d309e0f825e16ea2e2
Glenn Searby <glenn.searby@gmail.com>
2018-06-06 13:52:32 +1000

    emacs magit word diff enabled by default

--- .emacs.d/config/my-magit.el
+++ .emacs.d/config/my-magit.el
@@ -1,5 +1,6 @@
 (setq magit-push-always-verify nil)
 (setq magit-revert-buffers t) ; Automatically refresh affected buffers when magit makes changes.
+(setq magit-diff-refine-hunk t) ; Word diffs
 
 (setq vc-follow-symlinks nil)
 
