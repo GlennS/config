759972d64e48a9de41470ad9e0071656ea3d44ef
Glenn Searby <glenn.searby@gmail.com>
2021-11-13 22:03:52 +1100

    fix emacs agenda file selection on home computer

--- .emacs.d/config/my-org-agenda-files.el
+++ .emacs.d/config/my-org-agenda-files.el
@@ -1,9 +1,10 @@
 (setq org-agenda-files
-      (cond ((string-equal (system-name) "glaptoph") `("~/notes/schedule.org"
+      (cond ((string-equal (system-name) "GLAPTOPH") `("~/notes/schedule.org"
                                                        "~/notes/todo.org"))
 
             ('true `("~/notes/schedule.org"
                      "~/octopus/schedule.org"
                      "~/octopus/tasks"))))
+
 
 (provide `my-org-agenda-files)
