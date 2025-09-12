38ac1f4fa01d213d60fc8d353e683ff54cc664c6
Glenn Searby <glenn.searby@gmail.com>
2025-09-12 11:03:38 +1000

    autorandr

--- .screenlayout/home-dual-monitors.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-xrandr \
-    --output DP-3 --mode 1920x1200 --pos 0x0 --rotate normal \
-    --output DP-1 --mode 2560x1440 --pos 1920x0 --rotate normal --primary \
-    --output eDP-1 --mode 1920x1080 --pos 4480x0 --rotate normal
