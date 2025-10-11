cad392d3bfdf4140dbdee265ea85a7551ad987c7
Glenn Searby <glenn.searby@gmail.com>
2025-10-11 19:42:47 +1100

    set SSH_AUTH_SOCK using gpg agent

--- .emacs.d/config/my-magit.el
+++ .emacs.d/config/my-magit.el
@@ -1,3 +1,9 @@
+(when (executable-find "gpgconf")
+  (let* ((socket-path (string-trim (shell-command-to-string "gpgconf --list-dirs agent-ssh-socket"))))
+    (unless (string= socket-path "")
+      (setenv "SSH_AUTH_SOCK" socket-path)
+      (message "SSH_AUTH_SOCK set to: %s" socket-path))))
+
 (setq magit-push-always-verify nil)
 (setq magit-revert-buffers t) ; Automatically refresh affected buffers when magit makes changes.
 (setq magit-diff-refine-hunk t) ; Word diffs
