aadbae3c2fe5b57a6368f56e07d633a0e94873af
Glenn Searby <glenn.searby@gmail.com>
2025-10-12 15:44:38 +1100

    Remove some unnecessary ensures from use-package - I have auto-ensure on already

--- .emacs.d/config/my-magit.el
+++ .emacs.d/config/my-magit.el
@@ -1,12 +1,10 @@
 (use-package keychain-environment
-  :ensure t ; This tells use-package to automatically install the package if it's not found.
   :config
   (keychain-refresh-environment)
   ;; Optional: Periodically refresh the environment if Emacs runs for a long time
   (run-with-timer 300 300 'keychain-refresh-environment))
 
 (use-package magit
-  :ensure t ; Ensure Magit is installed.
   :bind (("C-c g" . magit-status)) ; Keybinding for magit-status.
 
   :config
@@ -19,7 +17,6 @@
   (setq vc-follow-symlinks nil))
 
 (use-package browse-at-remote
-  :ensure t
   :bind (("C-c C-g" . browse-at-remote)))
 
 (provide `my-magit)
