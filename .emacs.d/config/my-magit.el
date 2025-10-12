85dd689f168fe267a2f96ff5baced08faf810302
Glenn Searby <glenn.searby@gmail.com>
2025-10-12 14:30:06 +1100

    modernize magit config with use-package

--- .emacs.d/config/my-magit.el
+++ .emacs.d/config/my-magit.el
@@ -5,15 +5,21 @@
   ;; Optional: Periodically refresh the environment if Emacs runs for a long time
   (run-with-timer 300 300 'keychain-refresh-environment))
 
-(setq magit-push-always-verify nil)
-(setq magit-revert-buffers t) ; Automatically refresh affected buffers when magit makes changes.
-(setq magit-diff-refine-hunk t) ; Word diffs
+(use-package magit
+  :ensure t ; Ensure Magit is installed.
+  :bind (("C-c g" . magit-status)) ; Keybinding for magit-status.
 
-(setq vc-follow-symlinks nil)
+  :config
+  ;; Magit Settings
+  (setq magit-push-always-verify nil)
+  (setq magit-revert-buffers t) ; Automatically refresh affected buffers.
+  (setq magit-diff-refine-hunk t) ; Word diffs
 
-(global-set-key (kbd "C-c g") 'magit-status)
+  ;; General Version Control Settings (often relevant to Magit usage)
+  (setq vc-follow-symlinks nil))
 
-(require 'browse-at-remote)
-(global-set-key (kbd "C-c C-g") 'browse-at-remote)
+(use-package browse-at-remote
+  :ensure t
+  :bind (("C-c C-g" . browse-at-remote)))
 
 (provide `my-magit)
