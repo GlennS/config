23f3036eae31cee2a493191500289ab7f19922a3
Glenn Searby <glenn.searby@gmail.com>
2025-10-12 14:27:17 +1100

    Use the keychain environment in Emacs

--- .emacs.d/config/my-magit.el
+++ .emacs.d/config/my-magit.el
@@ -1,3 +1,10 @@
+(use-package keychain-environment
+  :ensure t ; This tells use-package to automatically install the package if it's not found.
+  :config
+  (keychain-refresh-environment)
+  ;; Optional: Periodically refresh the environment if Emacs runs for a long time
+  (run-with-timer 300 300 'keychain-refresh-environment))
+
 (setq magit-push-always-verify nil)
 (setq magit-revert-buffers t) ; Automatically refresh affected buffers when magit makes changes.
 (setq magit-diff-refine-hunk t) ; Word diffs
