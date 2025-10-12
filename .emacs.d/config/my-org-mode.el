5a9ec06ceadf5c4a5bd613d501d907a64c5b0210
Glenn Searby <glenn.searby@gmail.com>
2025-10-12 16:08:25 +1100

    Rework my-org-mode.el with use-packages

--- .emacs.d/config/my-org-mode.el
+++ .emacs.d/config/my-org-mode.el
@@ -1,42 +1,62 @@
-(setq org-todo-keywords
-      '((sequence "BLOCKED" "IN-PROGRESS" "TODO" "|" "DONE" "FAILED")))
+(use-package org
+  ;; --- Initial Settings & Global Hooks ---
+  :init
+  ;; Set the global keybinding for the Agenda.
+  (global-set-key (kbd "C-c a") 'org-agenda)
 
-(setq org-export-with-toc nil)
+  (require 'ob-core)
 
-(setq org-babel-load-languages
-      (quote ((awk . t)
-              (dot . t)
-              (R . t)
-              (python . t)
-              (ruby . t)
-              (js . t)
-              (ipython . t)
-              )))
+  ;; Use rjsx for JavaScript source blocks instead of the default js-mode.
+  (org-babel-set-lang-mode "js" 'rjsx-mode)
 
-(setq org-confirm-babel-evaluate nil)
+  ;; --- Configuration ---
+  :config
+  ;; Define the custom TODO keyword sequence.
+  (setq org-todo-keywords
+        '((sequence "BLOCKED" "IN-PROGRESS" "TODO" "|" "DONE" "FAILED")))
 
-(setq org-export-backends
-      (quote (ascii
-              beamer
-              html
-              icalendar
-              latex
-              md
-              texinfo)))
+  ;; Configure Org Babel (Source Code Execution)
+  (org-babel-do-load-languages
+    'org-babel-load-languages
+    '((awk . t)
+      (dot . t)
+      (R . t)
+      (ruby . t)
+      (js . t)))
 
-(global-set-key (kbd "C-c a") 'org-agenda)
+  ;; Suppress the confirmation prompt before evaluating Babel source blocks.
+  (setq org-confirm-babel-evaluate nil)
 
-;; Use rjsx instead of js-mode for JavaScript.
-(add-hook `org-mode-hook (lambda () (add-to-list `org-src-lang-modes `("js" . rjsx))))
+  ;; --- Export Configuration ---
 
-;; org-mode latex export using XeLaTeX
-(setq TeX-engine 'xetex)
-(setq org-latex-pdf-process '("xelatex --shell-escape -interaction nonstopmode -output-directory %o %f"))
+  ;; Disable the Table of Contents on export.
+  (setq org-export-with-toc nil)
 
-;; Some sensible latex defaults for org-mode export
-(setq org-latex-packages-alist '(("" "parskip" nil)
-                                 ("margin=2cm" "geometry" nil)))
+  ;; Explicitly declare which backends are active.
+  (setq org-export-backends
+        '(ascii beamer html icalendar latex md texinfo))
 
-(setq python-shell-interpreter "python3")
+  ;; Configure LaTeX export settings for XeLaTeX and common packages.
+  (setq
+    TeX-engine 'xetex
+    org-latex-pdf-process '("xelatex --shell-escape -interaction nonstopmode -output-directory %o %f")
+    org-latex-packages-alist '(
+      ;; Do not indent paragraphs, use vertical space between them.
+      ("" "parskip" nil)
+      ;; Set the page margins to 2cm.
+      ("margin=2cm" "geometry" nil))))
 
-(provide `my-org-mode)
+;; IPython disabled, because it seems to be broken
+;; (use-package ob-ipython
+;;   :after org
+;;   :config
+;;   ;; Add ipython to the list of languages loaded by Org Babel.
+;;   ;; This ensures it's initialized and ready to execute source blocks.
+;;   (org-babel-do-load-languages
+;;     'org-babel-load-languages
+;;     '((python . t)
+;;       (ipython . t)))
+
+;;   (setq python-shell-interpreter "python3"))
+
+(provide 'my-org-mode)
