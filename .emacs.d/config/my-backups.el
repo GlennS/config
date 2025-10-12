0a2d3ecb6cd72c2611d14e7e9634a9e6bba8f4f0
Glenn Searby <glenn.searby@gmail.com>
2025-10-12 14:27:35 +1100

    Re-enable emacs backups directory

--- .emacs.d/config/my-backups.el
+++ .emacs.d/config/my-backups.el
@@ -1,5 +1,10 @@
 (setq auto-save-default nil)
-(setq make-backup-files nil)
+
 (setq backup-directory-alist '(("." . "~/backups")))
+;; Don't put a bunch of files in the backup directory
+(setq delete-old-versions t
+      kept-old-versions 5
+      kept-new-versions 5
+      version-control t)
 
 (provide `my-backups)
