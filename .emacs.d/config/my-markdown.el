94610db4af7ba6ceb74416110a02307dbb15bf71
Glenn Searby <glenn.searby@gmail.com>
2025-10-12 15:36:33 +1100

    Moved my-markdown.el to use-package

--- .emacs.d/config/my-markdown.el
+++ .emacs.d/config/my-markdown.el
@@ -1,66 +1,108 @@
-(require `markdown-mode)
+(use-package markdown-mode
+  :defer t ; Defer loading until a markdown file is opened
+  :config
+  ;; --- Custom Functions for Table/Heading Movement and Manipulation ---
 
-(define-key markdown-mode-map (kbd "M-<return>") 'markdown-insert-header-dwim)
-(define-key markdown-mode-map (kbd "C-c C-l") 'markdown-insert-link)
+  (defun markdown-move-heading-or-row-up ()
+    "Move heading up, or move a table row up if at a table."
+    (interactive)
+    (if (markdown-table-at-point-p) (markdown-table-move-row-up)
+      (markdown-move-up)))
 
-(defun markdown-move-heading-or-row-up ()
-  (interactive)
-  (if (markdown-table-at-point-p) (markdown-table-move-row-up)
-    (markdown-move-up)))
+  (defun markdown-move-heading-or-row-down ()
+    "Move heading down, or move a table row down if at a table."
+    (interactive)
+    (if (markdown-table-at-point-p) (markdown-table-move-row-down)
+      (markdown-move-down)))
 
-(define-key markdown-mode-map (kbd "M-<up>") 'markdown-move-heading-or-row-up)
+  ;; ... [All other custom functions defined here] ...
 
-(defun markdown-move-heading-or-row-down ()
-  (interactive)
-  (if (markdown-table-at-point-p) (markdown-table-move-row-down)
-    (markdown-move-down)))
+  (defun markdown-delete-column-or-promote ()
+    "Delete table column, or promote a heading if not at a table."
+    (interactive)
+    (if (markdown-table-at-point-p) (markdown-table-delete-column)
+      (markdown-promote)))
 
-(define-key markdown-mode-map (kbd "M-<down>") 'markdown-move-heading-or-row-down)
+  ;; --- Keybindings ---
+  :bind
+  (("M-<return>" . 'markdown-insert-header-dwim)
+   ("C-c C-l"    . 'markdown-insert-link)
+   ;; Custom Functions
+   ("M-<up>"     . 'markdown-move-heading-or-row-up)
+   ("M-<down>"   . 'markdown-move-heading-or-row-down)
+   ;; ... [All other bindings here] ...
+   ("C-c C-|"    . 'markdown-table-convert-region)))
 
-(defun markdown-move-column-left-or-promote ()
-  (interactive)
-  (if (markdown-table-at-point-p) (markdown-table-move-column-left)
-    (markdown-promote)))
+(use-package markdown-mode
+  ;; :defer t is optional here, as :bind implicitly defers loading.
+  ;; We keep it for clarity that we want deferred loading.
+  :defer t
 
-(define-key markdown-mode-map (kbd "M-<left>") 'markdown-move-column-left-or-promote)
+  :config
+  ;; --- Custom Functions for Table/Heading Movement and Manipulation ---
 
-(defun markdown-move-column-right-or-demote ()
-  (interactive)
-  (if (markdown-table-at-point-p) (markdown-table-move-column-right)
-    (markdown-demote)))
+  (defun markdown-move-heading-or-row-up ()
+    "Move heading up, or move a table row up if at a table."
+    (interactive)
+    (if (markdown-table-at-point-p) (markdown-table-move-row-up)
+      (markdown-move-up)))
 
-(define-key markdown-mode-map (kbd "M-<right>") 'markdown-move-column-right-or-demote)
+  (defun markdown-move-heading-or-row-down ()
+    "Move heading down, or move a table row down if at a table."
+    (interactive)
+    (if (markdown-table-at-point-p) (markdown-table-move-row-down)
+      (markdown-move-down)))
 
-(defun markdown-delete-row-or-move-up ()
-  (interactive)
-  (if (markdown-table-at-point-p) (markdown-table-delete-row)
-    (markdown-move-up)))
+  (defun markdown-move-column-left-or-promote ()
+    "Move column left, or promote a heading if not at a table."
+    (interactive)
+    (if (markdown-table-at-point-p) (markdown-table-move-column-left)
+      (markdown-promote)))
 
-(define-key markdown-mode-map (kbd "M-S-<up>") 'markdown-delete-row-or-move-up)
+  (defun markdown-move-column-right-or-demote ()
+    "Move column right, or demote a heading if not at a table."
+    (interactive)
+    (if (markdown-table-at-point-p) (markdown-table-move-column-right)
+      (markdown-demote)))
 
-(defun markdown-add-row-or-move-down ()
-  (interactive)
-  (if (markdown-table-at-point-p) (markdown-table-insert-row)
-    (markdown-move-down)))
+  (defun markdown-delete-row-or-move-up ()
+    "Delete table row, or move heading up if not at a table."
+    (interactive)
+    (if (markdown-table-at-point-p) (markdown-table-delete-row)
+      (markdown-move-up)))
 
-(define-key markdown-mode-map (kbd "M-S-<down>") 'markdown-add-row-or-move-down)
+  (defun markdown-add-row-or-move-down ()
+    "Insert table row, or move heading down if not at a table."
+    (interactive)
+    (if (markdown-table-at-point-p) (markdown-table-insert-row)
+      (markdown-move-down)))
 
-(defun markdown-add-column-or-demote ()
-  (interactive)
-  (if (markdown-table-at-point-p) (markdown-table-insert-column)
-    (markdown-promote)))
+  (defun markdown-add-column-or-demote ()
+    "Insert table column, or promote a heading if not at a table."
+    (interactive)
+    (if (markdown-table-at-point-p) (markdown-table-insert-column)
+      (markdown-promote)))
 
-(define-key markdown-mode-map (kbd "M-S-<right>") 'markdown-add-column-or-demote)
+  (defun markdown-delete-column-or-promote ()
+    "Delete table column, or promote a heading if not at a table."
+    (interactive)
+    (if (markdown-table-at-point-p) (markdown-table-delete-column)
+      (markdown-promote)))
 
-(defun markdown-delete-column-or-promote ()
-  (interactive)
-  (if (markdown-table-at-point-p) (markdown-table-delete-column)
-    (markdown-promote)))
-
-(define-key markdown-mode-map (kbd "M-S-<left>") 'markdown-delete-column-or-promote)
-
-(define-key markdown-mode-map (kbd "C-c C-|") 'markdown-table-convert-region)
-
+  ;; --- Keybindings ---
+  :bind
+  (("M-<return>" . 'markdown-insert-header-dwim)
+   ("C-c C-l"    . 'markdown-insert-link)
+   ;; Custom Functions
+   ("M-<up>"     . 'markdown-move-heading-or-row-up)
+   ("M-<down>"   . 'markdown-move-heading-or-row-down)
+   ("M-<left>"   . 'markdown-move-column-left-or-promote)
+   ("M-<right>"  . 'markdown-move-column-right-or-demote)
+   ("M-S-<up>"   . 'markdown-delete-row-or-move-up)
+   ("M-S-<down>" . 'markdown-add-row-or-move-down)
+   ("M-S-<right>" . 'markdown-add-column-or-demote)
+   ("M-S-<left>"  . 'markdown-delete-column-or-promote)
+   ("C-c C-|"    . 'markdown-table-convert-region)))
 
 
 (provide `my-markdown)
