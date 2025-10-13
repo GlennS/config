2c15657c11ff039256f9a0f33d8353225b8fc483
Glenn Searby <glenn.searby@gmail.com>
2025-10-13 11:13:39 +1100

    switching org agenda files based on system name wasn't working

--- .emacs.d/config/my-org-agenda-files.el
+++ .emacs.d/config/my-org-agenda-files.el
@@ -1,10 +1,7 @@
 (setq org-agenda-files
-      (cond ((string-equal (system-name) "glaptopw2") `("~/notes/schedule.org"
-                                                       "~/notes/todo.org"))
-
-            ('true `("~/notes/schedule.org"
-                     "~/octopus/schedule.org"
-                     "~/octopus/tasks"))))
-
+      `("~/notes/schedule.org"
+        "~/notes/todo.org"
+        "~/octopus/schedule.org"
+        "~/octopus/tasks"))
 
 (provide `my-org-agenda-files)
