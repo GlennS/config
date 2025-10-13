5b25699055b210bd1c4200c2793edfa9117bd544
Glenn Searby <glenn.searby@gmail.com>
2025-10-13 16:43:26 +1100

    Modernize my-programming-languages.el with use-package

--- .emacs.d/config/my-programming-languages.el
+++ .emacs.d/config/my-programming-languages.el
@@ -1,72 +1,110 @@
-;;; Everything
-(dumb-jump-mode)
+;;; --- Global/Everything ---
+(use-package dumb-jump
+  :config
+  (dumb-jump-mode 1))
 
-;;; JavaScript
-(setq js-indent-level 2)
-(setq js-expr-indent-offset 2)
+;;; --- JavaScript (js2-mode, rjsx-mode) ---
+(use-package js2-mode
+  :defer t
+  :mode ("\\.js\\'"))
 
-;; Use Flycheck instead of js2/rjsx for error checking.
-(setq js2-mode-show-parse-errors nil)
-(setq js2-mode-show-strict-warnings nil)
-(add-hook 'rjsx-mode-hook #'flycheck-mode)
+(use-package rjsx-mode
+  :defer t
+  :config
+  ;; Indentation settings for js2/rjsx
+  (setq js-indent-level 2
+        js-expr-indent-offset 2)
 
-;; Don't add newlines when writing JavaScript
-(add-hook 'js-mode-hook
-          (lambda ()
-            (set (make-local-variable 'require-final-newline)
-                 nil)))
+  ;; Use Flycheck instead of js2/rjsx for error checking.
+  (setq js2-mode-show-parse-errors nil
+        js2-mode-show-strict-warnings nil)
 
-(add-to-list 'auto-mode-alist '("\\.js$" . rjsx-mode))
-(add-to-list 'auto-mode-alist '("\\.jsx$" . rjsx-mode))
 
-;; Use web-mode for editing web things and ReportLab templates
-(add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.php$" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.rml$" . web-mode))
+  :hook (('rjsx-mode . 'flycheck-mode) ; Use Flycheck instead of js2/rjsx for error checking.
+         ('js-mode . #'(lambda () ; Don't add newlines when writing JavaScript
+                         (set (make-local-variable 'require-final-newline)
+                              nil))))
 
-(setq web-mode-markup-indent-offset 4)
-(setq web-mode-css-indent-offset 4)
-(setq web-mode-code-indent-offset 4)
 
-(setq web-mode-engines-alist
-      '(("django" . "\\.html\\'")
-        ("django" . "\\.rml\\'")))
+  ;; Associate rjsx-mode with .js and .jsx files
+  :mode (("\\.js\\'" . rjsx-mode)
+         ("\\.jsx\\'" . rjsx-mode)))
 
-;;; Clojure
-;; Auto-complete for Clojure
-(add-hook 'clojure-mode-hook #'cider-mode)
-(add-hook 'cider-repl-mode-hook #'company-mode)
-(add-hook 'cider-mode-hook #'company-mode)
+;;; --- Web Modes (HTML, PHP, ReportLab/RML) ---
+(use-package web-mode
+  :defer t
+  :config
+  ;; Set indentation offsets
+  (setq web-mode-markup-indent-offset 4
+        web-mode-css-indent-offset 4
+        web-mode-code-indent-offset 4)
 
-(setq cider-cljs-lein-repl ; Make ClojureScript + Figwheel + Cider work
-      "(do (use 'figwheel-sidecar.repl-api) (start-figwheel!) (cljs-repl))")
+  ;; Custom engine configuration
+  (setq web-mode-engines-alist
+        '(("django" . "\\.html\\'")
+          ("django" . "\\.rml\\'")))
 
-;;; SQL
-;; https://github.com/alex-hhh/emacs-sql-indent
-(add-hook 'sql-mode-hook 'sqlind-minor-mode)
+  ;; Associate web-mode with files
+  :mode (("\\.html\\'" . web-mode)
+         ("\\.php\\'" . web-mode)
+         ("\\.rml\\'" . web-mode)))
 
-;;; Python
-(require 'eglot)
-(add-hook 'python-mode-hook 'eglot-ensure)
-(setq-default eglot-workspace-configuration
+;;; --- Clojure ---
+(use-package cider
+  :defer t
+  :hook ((clojure-mode . cider-mode)
+         (cider-repl-mode . company-mode)
+         (cider-mode . company-mode))
+  :config
+  ;; Make ClojureScript + Figwheel + Cider work
+  (setq cider-cljs-lein-repl
+        "(do (use 'figwheel-sidecar.repl-api) (start-figwheel!) (cljs-repl))"))
+
+;;; --- SQL ---
+(use-package sql-indent
+  :defer t
+  :hook (sql-mode . sqlind-minor-mode))
+
+;;; --- Python ---
+(use-package python
+  :defer t
+  :config
+  (setq python-indent-guess-indent-offset nil))
+
+(use-package eglot
+  :defer t
+  :hook (python-mode . eglot-ensure)
+  :config
+  (setq-default eglot-workspace-configuration
     '(:pylsp
       (:plugins
         (:pylsp_mypy
-          (:enabled t)))))
-(add-hook 'python-mode-hook #'company-mode)
-(setq python-indent-guess-indent-offset nil)
+          (:enabled t))))))
 
-(require 'ruffify)
-(add-hook 'python-mode-hook 'ruffify-mode)
+(use-package company
+  :defer t
+  :hook (python-mode . company-mode))
 
-;;; Latex
-(add-hook 'latex-mode-hook '(lambda () (flymake-mode)))
+(use-package ruffify
+  :defer t
+  :no-require t ; Tell use-package to just load the file, don't use 'require'
+  :ensure nil   ; Overrides the global always-ensure for this package
+  :hook (python-mode . ruffify-mode))
 
-;;; Nix
-(setq nix-indent-function (quote nix-indent-line))
+;;; --- Latex ---
+;; Flymake is built-in; no use-package needed for the hook.
+(add-hook 'latex-mode-hook #'(lambda () (flymake-mode)))
 
-;;; Terraform
-(add-hook `terraform-mode-hook #'flycheck-mode)
+;;; --- Nix ---
+(use-package nix-mode
+  :defer t
+  :config
+  (setq nix-indent-function 'nix-indent-line))
+
+;;; --- Terraform ---
+(use-package terraform-mode
+  :defer t
+  :hook (terraform-mode . flycheck-mode))
 
 ;;; Finish
-(provide `my-programming-languages)
+(provide 'my-programming-languages)
