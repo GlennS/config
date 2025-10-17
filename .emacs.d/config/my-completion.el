1299fe306c916d644acdc5928a10e1be59c058fa
Glenn Searby <glenn.searby@gmail.com>
2025-10-17 14:28:32 +1100

    Vertico-directory

--- .emacs.d/config/my-completion.el
+++ .emacs.d/config/my-completion.el
@@ -30,6 +30,17 @@
   (minibuffer-prompt-properties
    '(read-only t cursor-intangible t face minibuffer-prompt)))
 
+(use-package vertico-directory
+  :after vertico
+  :ensure nil
+  ;; More convenient directory navigation commands
+  :bind (:map vertico-map
+              ("RET" . vertico-directory-enter)
+              ("DEL" . vertico-directory-delete-char)
+              ("M-DEL" . vertico-directory-delete-word))
+  ;; Tidy shadowed file names
+  :hook (rfn-eshadow-update-overlay . vertico-directory-tidy))
+
 (use-package hotfuzz
   :config
   (setq completion-styles '(hotfuzz)))
